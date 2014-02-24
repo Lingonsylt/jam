e9daa7f517110d051a88a12f45d6cd758d6bffbe
Fredrik Areschoug <fredrik.areschoug@chas.se>
2014-02-24 15:08:54 +0100

    removed redundent forloop

--- animation_loader.py
+++ animation_loader.py
@@ -4,6 +4,7 @@ def load_animation(sprite_sheet):
     img = pyglet.image.load('sprite_sheets/'+sprite_sheet+'.png')
     sheet_meta = open('sprite_sheets/'+sprite_sheet+'_meta')
     animation_meta = []
+    animation = {}
 
     for line in sheet_meta:
         if line.startswith('quad'):
@@ -12,14 +13,8 @@ def load_animation(sprite_sheet):
             quad_height = int(line[2])
         else:
             line=line.split(':')
-            animation_meta.append([line[0],int(line[1]),int(line[2])])
-
-    ani_list = ()
-    animation = {}
-    for row in animation_meta:
-        frame_set=[]
-        for i in range(row[2]):
-            frame = img.get_region(i*quad_width, row[1]*quad_height, quad_width, quad_height)
-            animation.setdefault(row[0],[]).append(frame)
+            for i in range(int(line[2])):
+                frame = img.get_region(i*quad_width, int(line[1])*quad_height, quad_width, quad_height)
+                animation.setdefault(line[0],[]).append(frame)
 
     return img, animation
